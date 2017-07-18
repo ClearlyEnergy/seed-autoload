4a7e744b689d265a3ef153ec792dc586a3e2acf0
John Kastner <john.h.kastner@gmail.com>
2017-07-18 10:28:50 -0400

    Finished rewritting tests

--- autoload/test.py
+++ autoload/test.py
@@ -1,10 +1,13 @@
 """Test for the autoload module"""
 import autoload
 import StringIO
+import datetime
 
 from django.test import TestCase
 from django.utils import timezone
 from seed.landing.models import SEEDUser as User
+from seed.models.properties import PropertyState
+from seed.models.certification import GreenAssessment, GreenAssessmentProperty
 from seed.lib.superperms.orgs.models import Organization, OrganizationUser
 from seed.models import Cycle
 
@@ -33,7 +36,8 @@ class AutoloadTest(TestCase):
 
         self.loader = autoload.AutoLoad(self.user,self.org)
 
-    # Tests that individual method calls are successfull
+    # test that autoload returns with succes and that there exists a
+    # property state with the correct file_id
     def test_autoload(self):
         col_mappings = [{"from_field": "Address",
                      "to_field": "address_line_1",
@@ -47,9 +51,14 @@ class AutoloadTest(TestCase):
         dataset_name = 'TEST'
 
         resp = self.loader.autoload_file(file_handle,dataset_name,self.cycle.pk,col_mappings)
-        assertEqual(resp['status'],'success')
+        self.assertEqual(resp['status'],'success')
 
-    def _test_green_assessment_property(self):
+        file_id = resp['import_file_id']
+        self.assertTrue(PropertyState.objects.filter(import_file_id=file_id,address_line_1='123 Test Road').exists())
+
+    # test that upload of GreenAssessmentProperty succeds and that the
+    # new record can be found in the database
+    def test_green_assessment_property(self):
         col_mappings = [{"from_field": "Address",
                      "to_field": "address_line_1",
                      "to_table_name": "PropertyState",
@@ -61,6 +70,16 @@ class AutoloadTest(TestCase):
         file_handle = StringIO.StringIO('Address,klfjgkldsjg\n123 Test Road,100')
         dataset_name = 'TEST'
 
+        assessment = GreenAssessment.objects.create(
+                name='Home Energy Score',
+                award_body='Department of Energy',
+                recognition_type='SCR',
+                description='Developed by DOE...',
+                is_numeric_score=True,
+                is_integer_score=True,
+                validity_duration=datetime.timedelta(days=365),
+                organization=self.org)
+
         resp = self.loader.autoload_file(file_handle,dataset_name,self.cycle.pk,col_mappings)
         # check that the upload of initial data succeeds
         self.assertEqual(resp['status'],'success')
@@ -69,11 +88,13 @@ class AutoloadTest(TestCase):
         green_assessment = {"source":"home energy score",
                             "metric":10,
                             "date":"2017-07-10",
-                            "assessment":"1"}
+                            "assessment":assessment}
 
         resp = self.loader.create_green_assessment_property(file_id,green_assessment,self.org.pk,'123 Test Road')
 
         self.assertEqual(resp['status'],'success')
+
+        self.assertTrue(GreenAssessmentProperty.objects.filter(assessment=assessment,_metric=10,date='2017-07-10').exists())
 
     def test_is_string(self):
         print self
