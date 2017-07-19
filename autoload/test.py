9b7d85be41c433a413872a30d3976e301f81c0f6
John Kastner <john.h.kastner@gmail.com>
2017-07-19 10:25:54 -0400

    Wrote test for updating green_assessment

--- autoload/test.py
+++ autoload/test.py
@@ -36,6 +36,18 @@ class AutoloadTest(TestCase):
 
         self.loader = autoload.AutoLoad(self.user,self.org)
 
+        self.assessment = GreenAssessment.objects.create(
+                name='Home Energy Score',
+                award_body='Department of Energy',
+                recognition_type='SCR',
+                description='Developed by DOE...',
+                is_numeric_score=True,
+                is_integer_score=True,
+                validity_duration=datetime.timedelta(days=365),
+                organization=self.org)
+
+
+
     # test that autoload returns with succes and that there exists a
     # property state with the correct file_id
     def test_autoload(self):
@@ -70,15 +82,34 @@ class AutoloadTest(TestCase):
         file_handle = StringIO.StringIO('Address,klfjgkldsjg\n123 Test Road,100')
         dataset_name = 'TEST'
 
-        assessment = GreenAssessment.objects.create(
-                name='Home Energy Score',
-                award_body='Department of Energy',
-                recognition_type='SCR',
-                description='Developed by DOE...',
-                is_numeric_score=True,
-                is_integer_score=True,
-                validity_duration=datetime.timedelta(days=365),
-                organization=self.org)
+        resp = self.loader.autoload_file(file_handle,dataset_name,self.cycle.pk,col_mappings)
+        # check that the upload of initial data succeeds
+        self.assertEqual(resp['status'],'success')
+        file_id = resp['import_file_id']
+
+        green_assessment = {"source":"home energy score",
+                            "metric":10,
+                            "date":"2017-07-10",
+                            "assessment":self.assessment}
+
+        resp = self.loader.create_green_assessment_property(file_id,green_assessment,self.org.pk,'123 Test Road')
+
+        # check that GreenAssessmentProperty upload succeeds
+        self.assertEqual(resp['status'],'success')
+
+        self.assertTrue(GreenAssessmentProperty.objects.filter(assessment=self.assessment,_metric=10,date='2017-07-10').exists())
+
+    def test_green_assessment_property_upload(self):
+        col_mappings = [{"from_field": "Address",
+                     "to_field": "address_line_1",
+                     "to_table_name": "PropertyState",
+                    },
+                    {"from_field": "Score",
+                     "to_field": "energy_score",
+                     "to_table_name": "PropertyState",
+                    }]
+        file_handle = StringIO.StringIO('Address,klfjgkldsjg\n123 Test Road,100')
+        dataset_name = 'TEST'
 
         resp = self.loader.autoload_file(file_handle,dataset_name,self.cycle.pk,col_mappings)
         # check that the upload of initial data succeeds
@@ -88,17 +119,21 @@ class AutoloadTest(TestCase):
         green_assessment = {"source":"home energy score",
                             "metric":10,
                             "date":"2017-07-10",
-                            "assessment":assessment}
+                            "assessment":self.assessment}
 
         resp = self.loader.create_green_assessment_property(file_id,green_assessment,self.org.pk,'123 Test Road')
 
+        # check that GreenAssessmentProperty upload succeeds
         self.assertEqual(resp['status'],'success')
 
-        self.assertTrue(GreenAssessmentProperty.objects.filter(assessment=assessment,_metric=10,date='2017-07-10').exists())
+        self.assertTrue(GreenAssessmentProperty.objects.filter(assessment=self.assessment,_metric=10,date='2017-07-10').exists())
 
-# Things to test (sketch)
-# Success, i.e. test for succesful completion of entire process
-# Failure if column headers don't match and/or column names can't be mapped automatically
-# Failure if input format won't work
-# Test new records created (this probably duplicates existing test functionality in SEED)
-# Test new information merged into existing record (this probably also duplicates existing test functionality in SEED)
+        green_assessment_update = {"metric":11,
+                                   "date":"2017-07-19"}
+
+        resp = self.loader.create_green_assessment_property(file_id,green_assessment_update,self.org.pk,'123 Test Road')
+
+        # check that GreenAssessmentProperty update succeeds
+        self.assertEqual(resp['status'],'success')
+
+        self.assertTrue(GreenAssessmentProperty.objects.filter(assessment=self.assessment,_metric=11,date='2017-07-19').exists())
